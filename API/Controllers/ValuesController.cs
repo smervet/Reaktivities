c6c76bb32f0f3723abcf43ebeb5e9d7f6717dbcd
merve.tekin <merve.tekin@edelkrone.com>
2020-01-13 14:41:48 +0300

    Initial Commit

---
+++ API/Controllers/ValuesController.cs
@@ -0,0 +1,38 @@
+
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System;
+using Persistence;
+using Domain;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            this._context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Value>>> Get()
+        {
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Value>> Get(int id)
+        {
+            var value = await _context.Values.FindAsync(id);
+            return Ok(value);
+        }
+
+    }
+}
